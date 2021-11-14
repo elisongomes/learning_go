32e0c1c7325d495af937d858fe226d3e1f012c2c
Élison Gomes <elisongomes@users.noreply.github.com>
2021-11-13 22:48:20 -0300

    Basic: operators

---
+++ 01_basic/05_operators/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	number1 := 10
+	number1++
+	fmt.Println(number1)
+
+	number1 += 15
+	fmt.Println(number1)
+
+	number1 *= 3
+	fmt.Println(number1)
+
+	number1 /= 3
+	fmt.Println(number1)
+
+	number1 %= 3
+	fmt.Println(number1)
+
+}
