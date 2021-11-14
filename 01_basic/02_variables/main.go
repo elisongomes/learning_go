ec7fe3b7e35529485a232ab4e5921affa6f6b313
Élison Gomes <elisongomes@users.noreply.github.com>
2021-11-13 22:38:18 -0300

    Typo variables

--- 01_basic/02_variables/main.go
+++ 01_basic/02_variables/main.go
@@ -2,31 +2,31 @@ package main
 
 import "fmt"
 
-func main()  {
-	var variavel1 string = "Variável 1"
+func main() {
+	var variable1 string = "Variable 1"
 
-	// O go determina o tipo da variável pelo valor = Inferência de tipos
-	variavel2 := "Variável 2"
+	// The type of variable is defined by value = Type inference
+	variable2 := "Variable 2"
 
-	fmt.Println(variavel1)
-	fmt.Println(variavel2)
+	fmt.Println(variable1)
+	fmt.Println(variable2)
 
-	// Declaração de várias variáveis
-	var(
-		variavel3 string = "Variável 3"
-		variavel4 string = "Variável 4"
+	// Multiple variables declaration
+	var (
+		variable3 string = "Variable 3"
+		variable4 string = "Variable 4"
 	)
 
-	fmt.Println(variavel3)
-	fmt.Println(variavel4)
+	fmt.Println(variable3)
+	fmt.Println(variable4)
 
-	variavel5, variavel6 := "Variável 5", "Variável 6"
-	fmt.Println(variavel5, variavel6)
+	variable5, variable6 := "Variable 5", "Variable 6"
+	fmt.Println(variable5, variable6)
 
-	const constante1 string = "Constante 1"
-	fmt.Println(constante1)
+	const constant1 string = "Constant 1"
+	fmt.Println(constant1)
 
 	//Trocar valor das variáveis
-	variavel5, variavel6 = variavel6, variavel5
-	fmt.Println(variavel5, variavel6)
+	variable5, variable6 = variable6, variable5
+	fmt.Println(variable5, variable6)
 }
