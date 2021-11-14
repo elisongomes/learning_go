e43ae23d379b8a9f401a709d5f99134e7e869be0
Élison Gomes <elisongomes@users.noreply.github.com>
2021-11-14 18:08:39 -0300

    Basic: if else

---
+++ 01_basic/11_if_else/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func mySum(a, b int) int {
+	return a + b
+}
+func main()  {
+	fmt.Println("Control structures")
+
+	// If init; Variable only in IF scope
+	if sum1 := mySum(2, 3); (sum1 % 2) == 0 {
+		fmt.Println("even")
+	} else {
+		fmt.Println("odd")
+	}
+}
